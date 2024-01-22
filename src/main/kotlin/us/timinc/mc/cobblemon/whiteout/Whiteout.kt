4b90bc09a4e0992c796ab915519abf1fcfbe86fc
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-01-22 08:35:56 -0800

    initial prototype

---
+++ src/main/kotlin/us/timinc/mc/cobblemon/whiteout/Whiteout.kt
@@ -0,0 +1,23 @@
+package us.timinc.mc.cobblemon.whiteout
+
+import com.cobblemon.mod.common.api.events.CobblemonEvents
+import com.cobblemon.mod.common.api.events.battles.BattleFaintedEvent
+import com.cobblemon.mod.common.api.events.battles.BattleVictoryEvent
+import net.fabricmc.api.ModInitializer
+
+object Whiteout : ModInitializer {
+    const val MOD_ID = "cobblemon-whiteout"
+
+    override fun onInitialize() {
+        CobblemonEvents.BATTLE_FAINTED.subscribe { handleBattleFainted(it) }
+    }
+
+    private fun handleBattleFainted(battleFaintedEvent: BattleFaintedEvent) {
+        val killed = battleFaintedEvent.killed
+        val entity = killed.entity ?: return
+        val owner = entity.owner ?: return
+        if (killed.actor.pokemonList.all { it.health == 0 }) {
+            owner.kill()
+        }
+    }
+}
